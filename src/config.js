b470c4ab3c83198d6a202a90597331c108f863ae
Matthew Rougely <matthewrougelysr@gmail.com>
2020-08-10 20:46:13 -0700

    deploying

--- src/config.js
+++ src/config.js
@@ -1,6 +1,4 @@
 export default {
-API_ENDPOINT: 'http://localhost:8000/api',
-TOKEN_KEY: 'poll-data-client-auth-token',
-API_KEY: process.env.REACT_APP_API_KEY,
-ID_KEY: 'user_id'
+API_ENDPOINT: `https://fathomless-reef-21167.herokuapp.com/api`,
+API_KEY: process.env.REACT_APP_API_KEY
 }
