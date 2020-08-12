5111ba3296d2d355bd12cd0fd07c4a999417135a
Matthew Rougely <matthewrougelysr@gmail.com>
2020-08-11 17:28:16 -0700

    CONFIG CHANGES

--- src/config.js
+++ src/config.js
@@ -1,6 +1,15 @@
+// export default {
+// TOKEN_KEY: 'poll-data-client-auth-token',
+// API_ENDPOINT: `https://fathomless-reef-21167.herokuapp.com/api`,
+// API_KEY: process.env.REACT_APP_API_KEY,
+// ID_KEY: 'user_id'
+// }
+
 export default {
-TOKEN_KEY: 'poll-data-client-auth-token',
-API_ENDPOINT: `https://fathomless-reef-21167.herokuapp.com/api`,
-API_KEY: process.env.REACT_APP_API_KEY,
-ID_KEY: 'user_id'
-}
+    API_ENDPOINT: process.env.REACT_APP_API_HOST,
+    API_TOKEN: process.env.REACT_APP_API_KEY,
+    TOKEN_KEY: 'poll-data-client-auth-token',
+    ID_KEY: 'user_id',
+    JWT_SECRET: '106816c9-bcb5-4a05-9d51-4dda16a357c3',
+    }
+    
