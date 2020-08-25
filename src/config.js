67a4a936097942578aadf85aff0fff51fe9e2c61
Matthew Rougely <matthewrougelysr@gmail.com>
2020-08-24 20:39:40 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,7 @@
 export default {
-    API_ENDPOINT: process.env.REACT_APP_API_HOST,
-    API_TOKEN: process.env.REACT_APP_API_KEY,
-    TOKEN_KEY: 'poll-data-client-auth-token',
-    ID_KEY: 'user_id',
-    JWT_SECRET: process.env.REACT_APP_JWT_SECRET,
-    }
-    
+  API_ENDPOINT: process.env.REACT_APP_API_HOST,
+  API_TOKEN: process.env.REACT_APP_API_KEY,
+  TOKEN_KEY: "poll-data-client-auth-token",
+  ID_KEY: "user_id",
+  JWT_SECRET: process.env.REACT_APP_JWT_SECRET,
+};
